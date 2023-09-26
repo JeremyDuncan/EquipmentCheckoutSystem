5df57b7f02588791dacbc0a17eba081fb56fce70
JeremyDuncan <jeremy.duncan1984@gmail.com>
2023-09-25 23:29:02 -0400

    Added Dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  ##############################################################################
+  ## ROOT LOCATION ##
+  ###################
+  root "dashboard#index"
+  
+  ##############################################################################
+  ## MAIN ROUTES ##
+  #################
   resources :maintenance_staffs
   resources :management_staffs
   resources :hr_staffs
   resources :equipment_inventories
   resources :employee_records
   resources :reports
-
 end
