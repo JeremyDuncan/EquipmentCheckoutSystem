2baffd2677ca710ad49d44e58c6466c84c36f837
JeremyDuncan <jeremy.duncan1984@gmail.com>
2023-09-28 17:56:22 -0400

    updated relationships and validations

--- app/models/maintenance_staff.rb
+++ app/models/maintenance_staff.rb
@@ -1,7 +1,11 @@
 class MaintenanceStaff < ApplicationRecord
-  has_many :equipment_inventories, dependent: :nullify
+  has_many :equipment_inventories, foreign_key: 'maintenance_staffs_id', dependent: :nullify
   has_one :employee_record, as: :employee
-
+  # ======================================================================
+  # This validation ensures that both first_name and last_name are present 
+  # before saving the model.
+  # ----------------------------------------------------------------------
+  validates :first_name, :last_name, presence: true
 
   #TODO=> SETUP METHODS
   def full_name
