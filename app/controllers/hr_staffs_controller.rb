b6072910f3f79741d1e663632f0daf3a9e4c40cd
JeremyDuncan <jeremy.duncan1984@gmail.com>
2023-09-29 08:02:49 -0400

    added create_employee method

--- app/controllers/hr_staffs_controller.rb
+++ app/controllers/hr_staffs_controller.rb
@@ -11,17 +11,22 @@ class HrStaffsController < ApplicationController
     @hr_staff = HrStaff.new
   end
   def create
-    @hr_staff = HrStaff.new(hr_staff_params)
-    if @hr_staff.save
-      redirect_to hr_staffs_path, notice: 'HR Employee was successfully added.'
-    else
-      flash.now[:alert] = @hr_staff.errors.full_messages.join(", ")
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('alert', 
-                                                                        partial: 'turbo_layouts/alert', 
-                                                                        locals: { alert: flash.now[:alert] }) }
-        format.html { render :new }
+    create_employee
+  end
+  def create_employee
+    employee_type       = params[:hr_staff].delete(:employee_type).to_sym
+    employee_attributes = params.require(:hr_staff).permit(:first_name, :last_name)
+    begin
+      new_employee = EmployeeFactory.create_employee(employee_type, employee_attributes)
+      if new_employee.persisted?
+        redirect_to hr_staffs_path, notice: 'Employee was successfully created.'
+      else
+        flash.now[:alert] = new_employee.errors.full_messages.join(", ")
+        render :new
       end
+    rescue => e
+      flash.now[:alert] = e.message
+      render :new
     end
   end
   
